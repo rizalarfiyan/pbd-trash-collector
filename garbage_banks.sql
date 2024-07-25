78d3207678914eaa3ae617d6c157a71ad71012e7
Ahmad Mufied Nugroho <teranggelap01@gmail.com>
2024-07-26 01:56:57 +0700

    Add dummy data for garbage_banks, operation_hours tables

---
+++ garbage_banks.sql
@@ -0,0 +1,12 @@
+INSERT INTO garbage_banks (province_id, regency_id, district_id, village_id, address)
+VALUES
+('34', '3401', '340101', '3401010001', 'Jl. Malioboro No. 1, Yogyakarta, Daerah Istimewa Yogyakarta'),
+('34', '3402', '340201', '3402010002', 'Jl. Kaliurang No. 2, Sleman, Daerah Istimewa Yogyakarta'),
+('35', '3501', '350101', '3501010003', 'Jl. Tunjungan No. 3, Surabaya, Jawa Timur'),
+('35', '3502', '350201', '3502010004', 'Jl. Diponegoro No. 4, Malang, Jawa Timur'),
+('31', '3101', '310101', '3101010005', 'Jl. MH Thamrin No. 5, Jakarta Pusat, DKI Jakarta'),
+('31', '3102', '310201', '3102010006', 'Jl. Sudirman No. 6, Jakarta Selatan, DKI Jakarta'),
+('18', '1801', '180101', '1801010007', 'Jl. Imam Bonjol No. 7, Bandar Lampung, Lampung'),
+('18', '1802', '180201', '1802010008', 'Jl. Raden Intan No. 8, Metro, Lampung'),
+('16', '1601', '160101', '1601010009', 'Jl. Letjen Suprapto No. 9, Palembang, Sumatera Selatan'),
+('16', '1602', '160201', '1602010010', 'Jl. Sudirman No. 10, Lubuklinggau, Sumatera Selatan');
