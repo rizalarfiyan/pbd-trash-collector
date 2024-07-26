7bdab8bc048e8fa385901e36befc04ce8c7e6522
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-07-26 11:14:57 +0700

    feat: formatting in file view.sql
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- view.sql
+++ view.sql
@@ -1,13 +1,16 @@
-drop view if exists v_regency_3403;
-create view v_regency_3403 as select user_id, province_id, regency_id, district_id, village_id, address, city_born, born_date, gender from user_details where regency_id = '3403';
+DROP VIEW if EXISTS v_regency_3403;
+CREATE VIEW v_regency_3403 AS
+SELECT user_id, province_id, regency_id, district_id, village_id, address, city_born, born_date, gender FROM user_details
+WHERE regency_id = '3403';
 
-drop view if exists v_wilayah;
-create view v_wilayah as
-    SELECT p.province_id, r.regency_id, d.district_id, v.village_id, p.name AS province, r.name AS regency, d.name AS district, v.name AS village
-    FROM provinces p JOIN regencies r USING (province_id) JOIN districts d USING (regency_id) JOIN villages v USING (district_id);
+DROP VIEW if EXISTS v_wilayah;
+CREATE VIEW v_wilayah AS
+SELECT p.province_id, r.regency_id, d.district_id, v.village_id, p.name AS province, r.name AS regency, d.name AS district, v.name AS village
+FROM provinces p JOIN regencies r USING (province_id) JOIN districts d USING (regency_id) JOIN villages v USING (district_id);
 
-drop view if exists v_approved_carts;
-create view v_approved_carts as select * from carts where status = 'approved';
+DROP VIEW if EXISTS v_approved_carts;
+CREATE VIEW v_approved_carts AS
+SELECT * FROM carts WHERE status = 'approved';
 
-drop view if exists v_heavy_approved_carts;
-create view v_heavy_approved_carts as select * from v_approved_carts where total_weight > 10 with cascaded check option;
+DROP VIEW if EXISTS v_heavy_approved_carts;
+CREATE VIEW v_heavy_approved_carts AS SELECT * FROM v_approved_carts WHERE total_weight > 10 WITH CASCADED CHECK OPTION;
