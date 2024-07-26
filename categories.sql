6a39fcd3f1460e25fb825131fe5c872956f36dab
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-07-26 08:20:54 +0700

    feat: refactor seeding data sql
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- categories.sql
+++
@@ -1,9 +0,0 @@
-USE trash_collector;
-
-INSERT INTO categories (name, category_id) VALUES ('Plastik', 1);
-INSERT INTO categories (name, category_id) VALUES ('Kaleng', 2);
-INSERT INTO categories (name, category_id) VALUES ('Kertas', 3);
-INSERT INTO categories (name, category_id) VALUES ('Kaca', 4);
-INSERT INTO categories (name, category_id) VALUES ('Logam', 5);
-INSERT INTO categories (name, category_id) VALUES ('Organik', 6);
-INSERT INTO categories (name, category_id) VALUES ('Elektronik', 7);
---
+++ seeds/002_categories.sql
@@ -0,0 +1,40 @@
+INSERT INTO categories (category_id, name)
+VALUES (1, 'Plastik'),
+       (2, 'Kaleng'),
+       (3, 'Kertas'),
+       (4, 'Kaca'),
+       (5, 'Logam'),
+       (6, 'Organik'),
+       (7, 'Elektronik');
+
+INSERT INTO trashes (trash_id, name)
+VALUES (1, 'Botol Plastik'),
+       (2, 'Tas Plastik'),
+       (3, 'Kaleng Soda'),
+       (4, 'Kaleng Makanan'),
+       (5, 'Koran Bekas'),
+       (6, 'Karton'),
+       (7, 'Botol Kaca'),
+       (8, 'Gelas Pecah'),
+       (9, 'Paku'),
+       (10, 'Kawat'),
+       (11, 'Sisa Makanan'),
+       (12, 'Daun Kering'),
+       (13, 'Televisi Rusak'),
+       (14, 'Handphone Bekas');
+
+INSERT INTO category_trash (category_id, trash_id, category_id)
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (2, 4),
+       (3, 5),
+       (3, 6),
+       (4, 7),
+       (4, 8),
+       (5, 9),
+       (5, 10),
+       (6, 11),
+       (6, 12),
+       (7, 13),
+       (7, 14);
