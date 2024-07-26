e1b76f4576f6eece684e8d3eebf518f4c3bc50ff
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-07-26 09:41:08 +0700

    feat: update locations data seed
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- seeds/003_garbage_banks.sql
+++ seeds/003_garbage_banks.sql
@@ -1,14 +1,14 @@
 INSERT INTO garbage_banks (garbage_bank_id, province_id, regency_id, district_id, village_id, address)
-VALUES (1, '34', '3401', '340101', '3401010001', 'Jl. Malioboro No. 1, Yogyakarta, Daerah Istimewa Yogyakarta'),
-       (2, '34', '3402', '340201', '3402010002', 'Jl. Kaliurang No. 2, Sleman, Daerah Istimewa Yogyakarta'),
-       (3, '35', '3501', '350101', '3501010003', 'Jl. Tunjungan No. 3, Surabaya, Jawa Timur'),
-       (4, '35', '3502', '350201', '3502010004', 'Jl. Diponegoro No. 4, Malang, Jawa Timur'),
-       (5, '31', '3101', '310101', '3101010005', 'Jl. MH Thamrin No. 5, Jakarta Pusat, DKI Jakarta'),
-       (6, '31', '3102', '310201', '3102010006', 'Jl. Sudirman No. 6, Jakarta Selatan, DKI Jakarta'),
-       (7, '18', '1801', '180101', '1801010007', 'Jl. Imam Bonjol No. 7, Bandar Lampung, Lampung'),
-       (8, '18', '1802', '180201', '1802010008', 'Jl. Raden Intan No. 8, Metro, Lampung'),
-       (9, '16', '1601', '160101', '1601010009', 'Jl. Letjen Suprapto No. 9, Palembang, Sumatera Selatan'),
-       (10, '16', '1602', '160201', '1602010010', 'Jl. Sudirman No. 10, Lubuklinggau, Sumatera Selatan');
+VALUES (1, '34', '3401', '340103', '3401032007', 'GANTI JALAN'),
+       (2, '34', '3403', '340314', '3403142006', 'GANTI JALAN'),
+       (3, '34', '3471', '347111', '3471111001', 'GANTI JALAN'),
+       (4, '34', '3403', '340312', '3403122001', 'GANTI JALAN'),
+       (5, '34', '3471', '347112', '3471121001', 'GANTI JALAN'),
+       (6, '34', '3471', '347101', '3471011004', 'GANTI JALAN'),
+       (7, '34', '3402', '340201', '3402012001', 'GANTI JALAN'),
+       (8, '34', '3403', '340315', '3403152007', 'GANTI JALAN'),
+       (9, '34', '3403', '340314', '3403142007', 'GANTI JALAN'),
+       (10, '34', '3401', '340106', '3401062003', 'GANTI JALAN');
 
 INSERT INTO operation_hours (garbage_bank_id, day, open, close)
 VALUES (1, 'monday', '08:00:00', '17:00:00'),
